8489c5cf2a5fb07a4a3e08e554c53dff461ea54a
Radek <radekc@sintelix.com>
2021-02-14 15:18:47 +1030

    actually update the version number

--- msy-show-stock.user.js
+++ msy-show-stock.user.js
@@ -2,7 +2,7 @@
 // @description Will load stock status on the product list page - make sure to select your store from the drop down.
 // @name MSY Show Me Stock
 // @namespace sysKin-scripts
-// @version 1.1.10
+// @version 1.1.11
 // @match https://www.msy.com.au/**
 // @match https://msy.com.au/**
 // @grant none
